943de121f7d8b6fcd137fc5cfe8c872c9dacf769
docos <do.costaic@gmail.com>
2022-09-21 14:17:15 +0200

    Add QRCode.ts

--- src/lib/QRCode.ts
+++ src/lib/QRCode.ts
@@ -1,7 +1,14 @@
+import qrcode from 'qrcode'
 
 export class QRCode {
-    constructor(text: string){
+    private text: string;
 
+    constructor(text: string){
+        this.text = text
     }
-     
+
+    getCanvas(){
+        const canvas = document.getElementById('canvas')
+        return qrcode.toCanvas(canvas, this.text)
+    }
 }
