7162e4b7160d7c139a72be24a86c5a89ddfe8ab5
docos <do.costaic@gmail.com>
2022-09-21 14:47:04 +0200

    QRCode basic implementation

--- src/routes/qrcode/+page.svelte
+++ src/routes/qrcode/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+    import { QRCode } from '$lib/QRCode'
+    let canvas: HTMLCanvasElement
+    let text: string
+    // text = document.getElementById('text') as unknown as string
+    $: canvas = new QRCode(text).getCanvas()
+</script>
+
 <h1>QR Code Builder</h1>
+<input id=text value="Initial Value"/>
+<canvas bind:this={canvas}></canvas>
