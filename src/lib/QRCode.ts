831a9413fe437ed1ef1f6a90fe03a5c466168cf2
Cedric <cedigasser@gmail.com>
2022-09-22 11:02:32 +0200

    Render qr code as img

--- src/lib/QRCode.ts
+++ src/lib/QRCode.ts
@@ -2,18 +2,21 @@ import qrcode from 'qrcode'
 import { browser } from '$app/environment';
 
 export class QRCode {
-    private text: string;
-
-    constructor(text: string){
-        this.text = text
-    }
-
-    async getCanvas(){
+    async getCanvas(text: string): Promise<HTMLCanvasElement | null> {
         if (!browser) {
             return null
         }
-        const canvas = document.createElement('canvas')
-        await qrcode.toCanvas(canvas, this.text)
+
+        const canvas = await qrcode.toCanvas(text, { scale: 1 })
         return canvas
+    }
+
+    static async getDataUrl(text: string): Promise<string | null>{
+        if (!browser) {
+            return null
+        }
+
+        const dataUrl = await qrcode.toDataURL(text, { scale: 1 })
+        return dataUrl
     }
 }
--- src/routes/qrcode/+page.svelte
+++ src/routes/qrcode/+page.svelte
@@ -2,20 +2,23 @@
     import { QRCode } from '$lib/QRCode'
     import { TextInput } from '@svelteuidev/core'
 
-    let canvas: HTMLCanvasElement
+    let dataUrl: string
     let text: string
     // text = document.getElementById('text') as unknown as string
-    $: updateCanvas(text)
+    $: updateImage(text)
 
-    async function updateCanvas(newText: string) {
-        let newCanvas = await new QRCode(text).getCanvas()
-        if (newCanvas) {
-            canvas.replaceWith(newCanvas)
-            canvas = newCanvas
-        }
+    async function updateImage(newText: string) {
+        dataUrl = await QRCode.getDataUrl(newText) ?? ''
     }
 </script>
 
 <h1>QR Code Builder</h1>
 <TextInput bind:value={text}/>
-<canvas bind:this={canvas}></canvas>
+<img src={dataUrl} alt="Generated QR Code"/>
+
+<style>
+    img {
+        width: min(100%, 400px);
+        image-rendering: pixelated;
+    }
+</style>
