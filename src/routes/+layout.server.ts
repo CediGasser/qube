1813f7f6e95087b4606250ad3691196d894f1ed5
Cedric <cedigasser@gmail.com>
2025-03-04 17:35:40 +0100

    Removed dotenv dependency

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,10 @@
-import type { ServerLoad } from '@sveltejs/kit'
-import 'dotenv/config'
+import type { ServerLoad } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
 
 export const load: ServerLoad = async () => {
-  const rcon_proxy_server = process.env.RCON_WEBSOCKET_PROXY ?? 'ws://localhost:8765'
+	const rcon_proxy_server = env.RCON_WEBSOCKET_PROXY ?? 'ws://localhost:8765';
 
-  return {
-    rcon_proxy_server
-  }
-}
+	return {
+		rcon_proxy_server
+	};
+};
